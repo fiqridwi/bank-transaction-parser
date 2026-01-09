82768f91860d19f783d2c3a958a06dc8696ce9c3
Fiqri Dwi <nurfiqri23@gmail.com>
2026-01-09 22:50:48 +0700

    feat: Add favicon icon to metadata in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Bank Transaction Converter",
   description: "Convert bank transaction PDF statements to Excel format",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
